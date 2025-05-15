7873de3d9eb2fcf1df3549c8116150f56935e212
LeandroPanozzo <leandropanozzo1@gmail.com>
2025-05-15 19:16:21 -0300

    cambio en url de noticias

--- diario/src/pages/context/axiosConfig.js
+++ diario/src/pages/context/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://api.diarioelgobierno.ar/diarioback/', // Mantener tu configuración de URL
+    baseURL: 'https://api.diarioelgobierno.ar/diarioback/', // Mantener tu configuración de URL
     headers: {
         'Content-Type': 'application/json',
     },
